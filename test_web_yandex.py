fbe0ad393242b9f0606064a68fe417f5849ffc92
botakusok <botakusok@bk.ru>
2026-02-28 22:43:08 +0700

    first day

--- test_web_yandex.py
+++ test_web_yandex.py
@@ -1,6 +1,9 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 import time
 browser = webdriver.Chrome()
 browser.get("http://ya.ru")
+time.sleep(5)
+element = browser.find_element(By.CLASS_NAME, "gdNaxmEdBjnJz-cection__item-icon").click()
 time.sleep(5)
 browser.quit()
